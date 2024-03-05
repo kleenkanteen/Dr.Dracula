ba4063bc610566e9a5f2bc1c667fbc4780f56602
risingsunomi <vincentcastro@gmail.com>
2024-03-05 03:06:30 -0500

    building out upload form in card

--- frontend/src/components/uploadPDF.tsx
+++ frontend/src/components/uploadPDF.tsx
@@ -1,11 +1,77 @@
+"use client";
+import { useTheme } from "next-themes"
+
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
- 
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  file: z.instanceof(FileList).optional(),
+});
+
 export function UploadPDF() {
+  const { setTheme } = useTheme()
+
+  setTheme("dark");
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const fileRef = form.register("file");
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+
   return (
-    <div className="grid w-full max-w-sm items-center gap-1.5">
-      <Label htmlFor="blood-pdf">Upload PDF of blood test</Label>
-      <Input id="blood-pdf" type="file" />
-    </div>
+    <Card>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full p-10">
+          <CardHeader>
+            <CardTitle>Upload PDF</CardTitle>
+            <CardDescription>Upload your blood test results</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <FormField
+              control={form.control}
+              name="file"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>File</FormLabel>
+                    <FormControl>
+                      <Input id="blood-pdf" type="file" {...fileRef} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+          </CardContent>
+          <CardFooter>
+            <Button type="submit">Upload</Button>
+          </CardFooter>
+        </form>
+      </Form>
+    </Card>
   )
 }
