23f3b3a99a00385111c68ce26966e67822dcffb3
Sabih S <67878058+kleenkanteen@users.noreply.github.com>
2024-03-05 17:35:22 -0500

    Upload doc dev - but with the python files I forgot to add ("git add ." only added children of current directory) (#12)
    * python files I forgot to add ("git add ." only added children of current directory)

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,36 @@
-from typing import Union
+import os
+from typing import Union, Annotated
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi import FastAPI
+from fastapi import FastAPI, File, Form, UploadFile
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+@app.post("/blood-test")
+async def upload_pdf(
+    file: Annotated[bytes, File()],
+    ):
+    # dont have to save it locally as a file actually, just added it to test that we get the file from the frontend and I can open it properly
+    with open("bloods.pdf", "wb+") as file_object:
+        file_object.write(file)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    # add logic for extracting text from pdf and getting a dictionary
+    
+    # add logic to return the supabase pdf file of the report
+    
+    return {
+        "file_size": len(file),
+    }
